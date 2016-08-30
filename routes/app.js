4499321774c5d9e2059e5cad4edf18d99ffa1336
Noah Ramey <noahramey1997@gmail.com>
2016-08-30 08:53:25 -0700

    fix package.json and route to index.hbs and not node.hbs
    :alien:

--- routes/app.js
+++ routes/app.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/:userTitle?', function (req, res, next) {
-    res.render('node', {title: req.params.userTitle ? req.params.userTitle : ''});
-});
-
-router.post('/', function (req, res, next) {
-    var title = req.body.title;
-    res.redirect('/' + title);
+router.get('/', function (req, res, next) {
+    res.render('index');
 });
 
 module.exports = router;
