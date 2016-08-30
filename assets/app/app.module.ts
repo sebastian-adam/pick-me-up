031e66c0715cb8eeeadbcb6b4291722800c655a2
Noah Ramey <noahramey1997@gmail.com>
2016-08-30 11:07:30 -0700

    add signin functionality on front end and back end

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from "./app.component";
+import { SigninComponent } from "./auth/signin.component";
+import { AuthService } from './auth/auth.service';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule],
-    bootstrap: [AppComponent]
+    declarations: [AppComponent, SigninComponent],
+    imports: [BrowserModule, HttpModule],
+    bootstrap: [AppComponent],
+    providers: [AuthService]
 })
 export class AppModule {
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
@@ -22,5 +23,37 @@ router.post('/', function(req, res, next) {
     });
   });
 });
+
+router.post('/signin', function(req, res, next) {
+  User.findOne({email: req.body.email}, function(err, doc) {
+    if (err) {
+      return res.status(500).json({
+        title: "An error occurred",
+        error: err
+      });
+    }
+    if (!doc) {
+      return res.status(500).json({
+        title: "No user found",
+        error: {message: "User could not be found"}
+      });
+    }
+    if (req.body.password !== doc.password) {
+      return res.status(401).json({
+        title: "Could not sign you in",
+        error: {message: 'Invalid password'}
+      });
+    }
+    var token = jwt.sign({user: doc}, 'secret', {expiresIn: 7200});
+    res.status(200).json({
+      message: 'Success',
+      token: token,
+      userEmail: doc.email,
+      userName: doc.name,
+      userId: doc._id
+    });
+
+  });
+})
 
 module.exports = router;
