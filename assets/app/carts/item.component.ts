ba218d1a53e9613c6cd182bfc74044dbae365f87
Augustine von Trapp <Augustvontrapp@gmail.com>
2016-08-30 23:19:50 -0700

    items by cart_id implemented in cart service and basic menu-item component with expansion

---
+++ assets/app/carts/item.component.ts
@@ -0,0 +1,14 @@
+import { Input, Component } from "@angular/core";
+import {Item} from "./item";
+
+@Component({
+  selector: 'menu-item',
+  template: `
+    <div>
+      <h4>{{item.name}}....\${{item.price}}</h4>
+    </div>
+  `,
+})
+export class ItemComponent {
+  @Input() item: Item;
+}
