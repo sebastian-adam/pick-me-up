3c4a4f1f349d8be155c3e6aa80ebce6598ca2e2e
Noah Ramey <noahramey1997@gmail.com>
2016-08-30 17:58:42 -0700

    add item services

--- assets/app/carts/cart-detail.component.ts
+++ assets/app/carts/cart-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 
 import { Cart } from "./cart";
+import { Item } from "./item";
 import { CartService } from "./cart.service";
 
 @Component({
@@ -9,6 +10,7 @@ import { CartService } from "./cart.service";
     <article>
       <div>
         {{ cart.name }}
+        <button (ngClick)="showItems(cart)">Show More</button>
         <button>VOTE</button>
       </div>
     </article>
@@ -18,4 +20,17 @@ export class CartDetailComponent {
   @Input() cart:Cart;
 
   constructor(private cartService: CartService) {}
+
+  items: Item[] = [];
+
+  showItems(cart) {
+    this.cartService.getItems(cart)
+      .subscribe(
+        items => {
+          this.items = items;
+          this.cartService.items = items;
+        },
+        error => console.log('im an error')
+      );
+  }
 }
--- assets/app/carts/cart.service.ts
+++ assets/app/carts/cart.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import {Cart} from './cart';
+import {Item} from './item';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/Rx';
 
@@ -8,6 +9,7 @@ import 'rxjs/Rx';
 export class CartService {
 
   carts: Cart[] = [];
+  items: Item[] = [];
 
   constructor (private _http: Http){}
 
@@ -23,6 +25,21 @@ export class CartService {
           return obj;
         })
         .catch(error => Observable.throw(error));
+  }
+
+  getItems(cart){
+    var cartId = cart._id;
+    return this._http.get('http://localhost:3000/carts/menu-items?cart_id=' + cartId)
+    .map(response => {
+      const data = response.json().obj;
+      let obj: any[] = [];
+      for(let i = 0; i < data.length; i++){
+        let item = new Item(data[i].name, data[i].price, cartId);
+        obj.push(item);
+      }
+      return obj;
+    })
+    .catch(error => Observable.throw(error));
   }
 
 }
