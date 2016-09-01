a2ac85e773c0fa108d57dde7436046159cb0d551
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-09-01 11:56:59 -0700

    modify menu-list and menu-detail components

--- assets/app/users/menu-detail.component.ts
+++ assets/app/users/menu-detail.component.ts
@@ -1,15 +1,33 @@
 import { Input, Component } from "@angular/core";
-import { Order } from "../admin/order";
 import { Item } from "./item";
+import { Order } from "../admin/order";
+import { OrderService } from "../admin/order.service";
+
 
 @Component({
   selector: 'menu-item',
   template: `
     <div>
       <h4>{{item.name}}....\${{item.price}}</h4>
+      <form [formGroup]="myForm" (ngSubmit)="onSubmit(item)">
+        <button>Add to my Order</button>
+      </form>
     </div>
   `,
 })
 export class MenuDetailComponent {
   @Input() item: Item;
+
+  constructor(private orderService: OrderService) {}
+
+  onSubmit(item) {
+    const order = new Order(item.id, localStorage.getItem('userId'));
+    this.orderService.createOrder(order)
+    .subscribe(
+      response => {
+        console.log(response)
+      },
+      error => console.log('place order error')
+    );
+  }
 }
--- assets/app/users/menu-list.component.ts
+++ assets/app/users/menu-list.component.ts
@@ -1,15 +1,29 @@
-import { Input, Component } from "@angular/core";
-import { Order } from "../admin/order";
+import { Component, OnInit } from "@angular/core";
 import { Item } from "./item";
+import { Cart } from "./cart";
+import { CartService } from "./cart.service";
 
 @Component({
-  selector: 'menu-item',
+  selector: 'menu-list',
   template: `
-    <div>
-      <h4>{{item.name}}....\${{item.price}}</h4>
-    </div>
-  `,
+    <h1>this is the menu list!</h1>
+    <section>
+       <menu-detail *ngFor="let item of items" [item]="item"></menu-detail>
+    </section>
+  `
 })
-export class MenuListComponent {
-  @Input() item: Item;
+export class MenuListComponent implements OnInit {
+  constructor(private cartService: CartService) {}
+
+  items: Item[] = [];
+
+  ngOnInit() {
+    this.cartService.getItems(this.cartService.votedCart)
+      .subscribe(
+        items => {
+          this.items = items;
+        },
+        error => console.log('cannot get carts error')
+      );
+  }
 }
