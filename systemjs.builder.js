87283d455ef74270fc9105196bd4753c3cc05d01
Noah Ramey <noahramey1997@gmail.com>
2016-09-02 10:22:10 -0700

    heroku attempt

--- systemjs.builder.js
+++ systemjs.builder.js
@@ -4,7 +4,7 @@ var del = require('del');
 
 var builder = new Builder('public', 'public/systemjs.config.js');
 
-builder.bundle('app/boot.js', './public/js/app/boot.js', { minify: true, encodeNames: false })
+builder.bundle('app/boot.js', './public/js/app/boot.js', { minify: true, mangle: false, encodeNames: false })
     .then(function() {
         del(['./public/js/app/**/*.js', '!./public/js/app/**/{boot.js,*.html,*.htm,*.css}'])
             .then(function(paths) {
